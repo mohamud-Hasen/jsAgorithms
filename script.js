1b5b64a18e25d88c70094052019542dfc20e4e79
Mohamud E Hasen <mohmudes4@gmail.com>
2025-01-16 09:19:07 -0600

    callbacks, assync, await and promises in js

--- script.js
+++ script.js
@@ -775,18 +775,70 @@
 // };
 // calculateTempreture(40);
 
-const gameProfile = {
-  userName: "seemore",
-  level: 5,
-  isOnline: false,
-};
+// const gameProfile = {
+//   userName: "seemore",
+//   level: 5,
+//   isOnline: false,
+// };
 
-const updateOnlineStatus = (profile, status) => {
-  profile.isOnline = status;
-  if (status) {
-    console.log(`${profile.userName} is now online`);
-  } else {
-    console.log(`${profile.userName} is now online`);
-  }
-};
-updateOnlineStatus(gameProfile);
+// const updateOnlineStatus = (profile, status) => {
+//   profile.isOnline = status;
+//   if (status) {
+//     console.log(`${profile.userName} is now online`);
+//   } else {
+//     console.log(`${profile.userName} is now online`);
+//   }
+// };
+// updateOnlineStatus(gameProfile);
+
+const posts = [
+  { title: "post one", body: "this is post one" },
+  { title: "post two", body: "this is post two" },
+];
+
+// function getPosts() {
+//   setTimeout(() => {
+//     let output = "";
+//     posts.forEach((post, index) => {
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//   }, 2000);
+// }
+
+// function createPost(post, callback) {
+//   setTimeout(() => {
+//     posts.push(post);
+//     callback();
+//   }, 2000);
+// }
+
+// createPost({ title: "post three", body: "this is post three" }, getPosts);
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output = `<li>${post.title}</>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      const error = true;
+      if (!error) {
+        resolve();
+      } else {
+        reject("error: something went wrong");
+      }
+    }, 2000);
+  });
+}
+
+createPost({ title: "post three", body: "this is post three" })
+  .then(getPosts)
+  .catch((err) => console.log(err));
